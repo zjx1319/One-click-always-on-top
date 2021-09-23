6ac39116baa0cf72629de3a2d453c900b39f2d43
zjx1319 <897223895@qq.com>
2021-09-23 23:12:04 +0800

    first commit

---
+++ One-click always on top.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+#include <iostream>
+using namespace std;
+#define ID 1
+
+int main() {
+    // 注册热键 ~
+    if (0 == RegisterHotKey(NULL, ID, NULL, VK_OEM_3)) {
+        cout << "RegisterHotKey error : " << GetLastError() << endl;
+        system("pause");
+        return 0;
+    }
+    cout << "Please switch to a window" << endl;
+    cout << "Then click '~' on your keyboard and the window will be always on top" << endl;
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        if (WM_HOTKEY == msg.message && ID == msg.wParam) {
+            HWND h = GetForegroundWindow();
+            if (SetWindowPos(h, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE + SWP_NOSIZE)) {
+                cout << "Success!" << endl;
+            }
+            else {
+                cout << "Fail!" << endl;
+            }
+            UnregisterHotKey(NULL, ID);
+            Sleep(3000);
+            break;
+        }
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+#One-click always on top
+![C++](C++.svg)
+[![Windows](Microsoft-Windows.svg)](https://www.microsoft.com/en-ie/windows)
+
+##Introduction
+This is a program that can help you put any window on top, running on Windows.
